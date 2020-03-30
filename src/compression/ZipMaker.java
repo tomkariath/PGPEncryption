ff090e5c29e8ea6daf432d87f9b993c45e7ba1d7
tomkariath <tomkariath2.0@gmail.com>
2020-03-31 00:14:52 +0530

    DECompression

--- src/compression/ZipMaker.java
+++ src/compression/ZipMaker.java
@@ -1,6 +1,8 @@
 package compression;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -9,6 +11,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class ZipMaker {
@@ -85,8 +88,58 @@ public class ZipMaker {
 		}
 	}
 	
+	public static void unzipFile (String zipFilePath) {
+		String destinationPath = zipFilePath.substring(0, zipFilePath.lastIndexOf(".zip"));
+		System.out.println(destinationPath);
+		File destinationDir = new File(destinationPath);
+		if (!destinationDir.exists()) {
+			destinationDir.mkdir();
+		}
+		
+		try {
+			ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(zipFilePath));
+			ZipEntry zipEntry = zipInputStream.getNextEntry();
+			
+			while(zipEntry!=null) {
+				String filePath = destinationPath + File.separator + zipEntry.getName();
+				
+				if (zipEntry.isDirectory()) {
+					File directory = new File(filePath);
+					directory.mkdirs();
+				}
+				else {
+					extract(zipInputStream, filePath);
+				}
+				zipInputStream.closeEntry();
+				zipEntry = zipInputStream.getNextEntry();
+			}
+			zipInputStream.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void extract (ZipInputStream zipInputStream, String filePath) throws IOException {
+		new File(new File(filePath).getParent()).mkdirs();
+		BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(filePath));
+		byte[] inputBytes = new byte[4096]; //Buffer Size
+		
+		int read = 0;
+		while((read = zipInputStream.read(inputBytes)) != -1) {
+			bufferedOutputStream.write(inputBytes, 0, read);
+		}
+		
+		bufferedOutputStream.close();
+	}
+	
 	public static void main(String[] args) {
-		zipFiles("D:\\Downloads\\Home Page.png", "D:\\Downloads\\Form Page.png");
-		zipDirectory("D:\\Downloads\\WorkDocs\\testFolder");
+		/*
+		 * zipFiles("D:\\Downloads\\Home Page.png", "D:\\Downloads\\Form Page.png");
+		 * zipDirectory("D:\\Downloads\\WorkDocs\\testFolder");
+		 */
+		unzipFile("D:\\Downloads\\WorkDocs\\testFolder.zip");
 	}
 }
